d98e1b0186f5842dea66ed473ea142b180dfc7f1
gmane <gmane.fr@gmail.com>
2025-05-17 14:14:26 +0100

    version pagina web con sub paginas

--- web_personal/app/routes.py
+++ web_personal/app/routes.py
@@ -1,6 +1,11 @@
 from app import app
-from flask import render_template
+from flask import render_template, abort
+import os
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+@app.route("/mi_primera_pagina_web")
+def mi_primera_pagina():
+	return render_template("Mi_primera_pagina_web.html")
